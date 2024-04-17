713b0cb159467f06c6655fa9671ff8b8dbcf1fef
Guiilhermendes <guilhermemendeds@gmail.com>
2024-04-17 19:13:24 -0300

    using 'url.searchParams' to save values of searchInput and send to back

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -22,35 +22,56 @@ interface Attendee {
 
 export function AttendeeList() {
     const [search, setSearch] = useState("")
-    const [page, setPage] = useState(1)
+    const [page, setPage] = useState(() => {
+        const url = new URL(window.location.toString())
+
+        if (url.searchParams.has('page')) return Number(url.searchParams.get('page'))
+        return 1
+    })
+
+    const [total, setTotal] = useState(0)
     const [attendees, setAttendees] = useState<Attendee[]>([])
 
     useEffect(() => {
-        fetch('http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees')
-            .then(response => response.json())
-            .then(data => setAttendees(data.attendees))
-    }, [page])
+        const url = new URL('http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees')
+        url.searchParams.set('pageIndex', String(page - 1))
+        if (search.length > 0) url.searchParams.set('query', search)
 
-    const totalPages = Math.ceil(attendees.length / 10)
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                setAttendees(data.attendees)
+                setTotal(data.total)
+            })
+    }, [page, search])
+
+    const totalPages = Math.ceil(total / 10)
+
+    function setCurrentPage(page: number) {
+        const url = new URL(window.location.toString())
+        url.searchParams.set("page", String(page))
+        window.history.pushState({}, "", url)
+        setPage(page)
+    }
 
     function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
         setSearch(event.target.value)
     }
 
     function goToFirstPage() {
-        setPage(1)
+        setCurrentPage(1)
     }
 
     function goToPreviousPage() {
-        setPage(page - 1)
+        if (page != 1) setCurrentPage(page - 1)
     }
 
     function goToNextPage() {
-        setPage(page + 1)
+        if (page != totalPages) setCurrentPage(page + 1)
     }
     
     function goToLastPage() {
-        setPage(totalPages)
+        setCurrentPage(totalPages)
     }
 
     return (
@@ -59,7 +80,11 @@ export function AttendeeList() {
                 <h1 className="text-2xl font-bold">Participantes</h1>
                 <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg flex items-center gap-3">
                     <Search className="size-4 text-emerald-300" />
-                    <input onChange={onSearchInputChanged} className="bg-transparent flex-1 outline-none border-0 p-0 text-sm" placeholder="Buscar participantes..." />
+                    <input 
+                        onChange={onSearchInputChanged}
+                        className="bg-transparent flex-1 outline-none border-0 p-0 text-sm focus:ring-0"
+                        placeholder="Buscar participantes..."
+                    />
                 </div>
                 {search}
             </div>
@@ -93,7 +118,12 @@ export function AttendeeList() {
                                         </div>
                                     </TableCell>
                                     <TableCell>{dayjs().to(attendee.createdAt)}</TableCell>
-                                    <TableCell>{dayjs().to(attendee.checkedInAt)}</TableCell>
+                                    <TableCell>
+                                        {attendee.checkedInAt === null 
+                                            ? <span className="text-zinc-400">Não fez check-in</span>
+                                            : dayjs().to(attendee.checkedInAt)
+                                        }
+                                    </TableCell>
                                     <TableCell>
                                         <IconButton transparent>
                                             <MoreHorizontal className="size-4" />
@@ -107,7 +137,7 @@ export function AttendeeList() {
                 <tfoot>
                     <tr>
                         <TableCell colSpan={3}>
-                            Mostrando 10 de {attendees.length} itens
+                            Mostrando {attendees.length} de {total} itens
                         </TableCell>
                         <TableCell className="text-right" colSpan={3}>
                             <div className="inline-flex items-center gap-8">
